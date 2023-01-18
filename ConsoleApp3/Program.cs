4ce07872f2d45e79f31fc3fbf9086744e47e5120
GustavJohansson420 <gujo04002@utb.vaxjo.se>
2023-01-18 10:16:37 +0100

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;    // Nästa så namnger jag uppgiften
+namespace Uppgift_3    // Class program är en datastruktur i C# som kombinerar datavariabler och funktioner till en enda enhet
+{
+    class Program    //static void etc är den första metoden som anropas när en applikation startas och den finns i varje körbar C#-fil.
+    {
+        static void Main(string[] args)   //På de nästa raderna är det jag väljer vad programmet ska skriva ut för något. 
+        {
+            Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" / \"\" \\");
+            Console.WriteLine("/______\\");
+        }
+    }
+}
